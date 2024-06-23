6972bdefb67f9607d6b0aa84fb1c66b074569114
Rayan Karki <rayankarki567@gmail.com>
2024-06-23 14:10:23 +0545

    Text aligned

--- app/Components/Selfbout.js
+++ app/Components/Selfbout.js
@@ -28,7 +28,7 @@ const SelfAbout = () => {
           </div>
         </div>
 
-        <div className='text-gray-600 items-start justify-center w-1/2 mt-28'>
+        <div className='text-gray-600 items-start justify-center text-center w-1/2 mt-28'>
           <div className='mt-4 ml-8'>Student at St. Xavier's College, Maitighar, Kathmandu</div>
           <div>Amet aute dolore minim duis magna mollit quis ex nulla qui eiusmod do nulla. Magna veniam cupidatat fugiat aliqua occaecat adipisicing duis enim pariatur ea. Adipisicing do ad eu proident. Pariatur excepteur voluptate deserunt adipisicing. Ad officia fugiat deserunt elit ut ea amet deserunt magna. Proident non cillum cillum pariatur tempor cillum ex.</div>
         </div>
