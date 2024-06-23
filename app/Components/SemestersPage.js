da403e971f4ffc72e0100ad6c788acbed5347df1
Rayan Karki <rayankarki567@gmail.com>
2024-06-23 17:54:59 +0545

    Sempg

--- app/Components/SemestersPage.js
+++ app/Components/SemestersPage.js
@@ -17,7 +17,7 @@ const FlippingCardBack = dynamic(() =>
 );
 
 const SemestersPage = () => {
-  const titles = ["Sem 1", "Sem 2", "Sem 3", "Sem 4"];
+  const titles = ["Sem 1", "Sem 2", "Sem 3", "Sem 4", "Sem 5"];
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
@@ -31,22 +31,18 @@ const SemestersPage = () => {
   return (
     <section className="min-h-screen p-12">
       <div className="container mx-auto flex justify-center">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-x-8">
           {titles.map((title, index) => (
             <FlippingCard key={index} className="h-60">
               <FlippingCardFront>
-                <div className="bg-gradient-to-b from-gray-200 to-gray-50 p-8 h-60 w-60 flex flex-col justify-center items-center rounded">
-                  <div className='bg-gradient-to-b from-dblue to-purple-200 rounded-md p-2 h-full w-full flex justify-center items-center'>
-                  <div className='bg-gradient-to-b from-gray-200 to-gray-50 rounded-md p-4 h-full w-full flex justify-center items-center'>
-                    <div className="text-2xl font-extrabold text-dblue ">{title}</div>
-                  </div>
-                  </div>
+                <div className="bg-gradient-to-b from-blue-200 to-white p-8 h-60 w-60 flex flex-col justify-center items-center rounded">
+                  <div className="text-2xl font-extrabold text-dblue ">{title}</div>
                 </div>
               </FlippingCardFront>
               <FlippingCardBack>
-                <div className="bg-gradient-to-b from-gray-200 to-gray-50 p-4 h-72 w-60 flex flex-col justify-center items-center rounded">
-                  <p className="text-gray-700 mt-2 text-center">Detailed information about {title}.</p>
-                  <a href="#" className="bg-gray-800 text-white font-medium px-4 py-2 rounded-md inline-block mt-4 hover:bg-white hover:text-gray-800 transition duration-300">View detail</a>
+                <div className="bg-gradient-to-b from-blue-200 to-gray-50 p-4 h-64 w-60 flex flex-col justify-center items-center shadow-xl rounded">
+                  {/* <p className="text-gray-700 mt-2 text-center">Detailed information about {title}.</p> */}
+                  <a href="#" className="bg-gradient-to-b from-blue-500 to-blue-200 text-white text-lg font-semibold rounded-xl px-6 py-4 transition-transform duration-300 ease-in-out hover:scale-110">Drive Link</a>
                 </div>
               </FlippingCardBack>
             </FlippingCard>
