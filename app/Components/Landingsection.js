1f1b93d5acae3167d89e438ec3034fc14b95758b
Rayan Karki <rayankarki567@gmail.com>
2024-06-26 19:22:26 +0545

    Link updated

--- app/Components/Landingsection.js
+++ app/Components/Landingsection.js
@@ -17,7 +17,7 @@ const Landingsection = () => {
           <div className="col-xs-12">
             <div className="sm:text-3xl md:text-5xl mb-2 font-bold">print("Hello World!")</div>
             <div className="sm:text-md md:text-xl mb-6 font-light">Nice to meet you. <TypedText /></div>
-            <Link href="/app/Drawings">
+            <Link href="../Drawings">
               <button className="border-2 border-white rounded-xl px-4 py-2 transition-transform duration-300 ease-in-out hover:scale-110 hover:bg-white hover:text-dblue">
                 <div className="sm:text-md md:text-xl font-medium m-1">Wanna Know More</div>
               </button>
