272e6978fb3f5f3158e7f9aaba5d753976980ad0
Rayan Karki <119793646+rayankarki567@users.noreply.github.com>
2024-09-23 10:43:03 +0545

    og tags

--- app/layout.js
+++ app/layout.js
@@ -4,13 +4,18 @@ import Navbar from "./Components/Navbar";
 export const metadata = {
   title: "Keh Website!",
   description: "Welcome to Keh Website. It's just sthg like a portfolio (By Rayan Karki)",
+  image: "/ss_landing.png", 
+  type: "website",  
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta property="og:image" content="/ss_landing.png" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.image} />
+        <meta property="og:type" content={metadata.type} />
       </head>
       <body>
         <Navbar />
