656f968f5ef4c1bed3663a63169a0ae529abd637
Rayan Karki <rayankarki567@gmail.com>
2024-12-29 17:23:07 +0545

    Google sign

--- app/login/page.js
+++ app/login/page.js
@@ -23,6 +23,17 @@ export default function LoginPage() {
     }
   };
 
+  const handleGoogleLogin = async() =>{
+    try{
+      const { error } = await supabase.auth.signInWithOAuth({provider: 'google'});
+      if(error){
+        setMsg("Google sign-in failed:" + error.message);
+      }
+    } catch(error){
+      setMsg("An unexpected error occured:" + error.message);
+    }
+  };
+
   return (
     <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
       <h1 className='text-2xl font-bold mb-4'>Login</h1>
@@ -42,12 +53,8 @@ export default function LoginPage() {
         className="mb-2 p-2 border border-gray-300 rounded"
         required
       />
-      <button 
-        onClick={handleLogin} 
-        className='p-2 bg-blue-500 text-white rounded'
-      >
-        Login
-      </button>
+      <button onClick={handleLogin} className='p-2 bg-blue-500 text-white rounded'>Login</button>
+      <button onClick={handleGoogleLogin} className='mt-4 p-2'>Sign in with Google</button>
       {msg && <p className='mt-4 text-red-500'>{msg}</p>}
     </div>
   );
