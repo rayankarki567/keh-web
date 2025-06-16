60c0611c91caeec97f9d4c9dad8ec13d0b0de8fa
Rayan Karki <119793646+rayankarki567@users.noreply.github.com>
2025-06-16 09:34:28 +0545

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,13 +10,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+    <title>{metadata.title}</title>
+
+    
+        <meta property="og:site_name" content="Keh Website" />
         <meta property="og:image" content="/ss_landing.png" />
+        <meta property="og:title" content={metadata.title} /> 
+        <meta property="og:url" content="https://www.rayankarki.com.np" />
+        <meta property="og:description" content={metadata.description} />
+    
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content="portfolio, Mr Keh, Keh website, Keh, Rayan Karki, Rayan, Karki, Rayan portfolio, Rayan Karki website, creative portfolio" />
         <meta name="author" content="Rayan Karki" /> 
-        <meta property="og:title" content={metadata.title} /> 
-        <meta property="og:description" content={metadata.description} />
         <meta name="google-site-verification" content="Z7PnaEwYZ_BdYbDI1yEDzxuSjfUpAX3OyLueVztpPvE" />
+    
         <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
         <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
         <script src="https://accounts.google.com/gsi/client" async defer></script>
