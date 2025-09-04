4f6dfe583373dae6027cf4f059dae87ad5d7aa92
Rayan Karki <rayankarki567@gmail.com>
2025-09-04 14:28:36 +0545

    Edited the title

--- app/sxc/page.js
+++ app/sxc/page.js
@@ -2,11 +2,11 @@ import React from 'react'
 import SemestersPage from '../Components/SemestersPage'
 
 export const metadata = {
-  title: "St. Xavier's College Semester Notes - Rayan Karki in Keh Website",
+  title: "SXC - Keh Website",
   description: "Access comprehensive semester notes and academic resources from St. Xavier's College. Study materials and notes compiled by Rayan Karki for fellow students in Keh Website.",
   keywords: ["St Xavier's College", "semester notes", "academic notes", "study materials", "college resources", "Nepal", "SXC", "education", "Rayan Karki", "Keh Website"],
   openGraph: {
-    title: "SXC Semester Notes",
+    title: "SXC Semester Notes - Keh Website",
     description: "Comprehensive academic notes and study materials from St. Xavier's College, compiled and shared by Rayan Karki in his site called Keh Website.",
     url: "https://www.rayankarki.com.np/sxc",
     type: "website"
