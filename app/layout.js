23bd5a1de759063aa848f29fef05bac1a1eb929d
Rayan Karki <rayankarki567@gmail.com>
2025-09-05 13:58:16 +0545

    GTMConfig

--- app/layout.js
+++ app/layout.js
@@ -135,6 +135,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager (head) */}
+        {process.env.NEXT_PUBLIC_GTM_ID && (
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+            `
+          }} />
+        )}
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         
@@ -200,6 +212,13 @@ export default function RootLayout({ children }) {
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        {process.env.NEXT_PUBLIC_GTM_ID && (
+          <noscript>
+            <iframe src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+                    height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+          </noscript>
+        )}
         <Navbar />
         <GoogleAnalytics />
         {children}
